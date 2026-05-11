780f1b792c251ef1cdc1e24da0141588302defb2
William Khan <william.khan@devineco.com>
2026-05-11 18:42:08 -0500

    Updated mapping module.

--- src/erddap_grid_comparer/mapping.py
+++ src/erddap_grid_comparer/mapping.py
@@ -1,33 +1,81 @@
+from typing import Self
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
+import pandas as pd
 
-def make_map_axes(source_df, lon_pad=0.2, lat_pad=0.2, figsize=(10, 8)):
+class PlotBuilder:
+    def __init__(
+            self, 
+            source_df: pd.DataFrame,
+            from_vehicle=False,
+            lon_pad=3,
+            lat_pad=3,
+            fig_size=(10,8),
+            track_col_names: list = ('precise_lon','precise_lat')
+    ):
+        self.source_df = source_df
+        self.from_vehicle = from_vehicle
+        self.lon_pad=lon_pad
+        self.lat_pad=lat_pad
+        self.fig_size=fig_size
+        self.track_col_names = track_col_names 
+        self.fig = None
+        self.ax = None
+        self.track = None
 
-    lon_col = next(column for column in source_df.columns if column.lower().startswith("precise_lon"))
-    lat_col = next(column for column in source_df.columns if column.lower().startswith("precise_lat"))
+    def configure_axes(self) -> Self:
 
-    track = source_df[[lon_col, lat_col]].dropna()
+        self.fig = plt.figure(figsize=self.fig_size)
+        self.ax = plt.axes(projection=ccrs.PlateCarree())
 
-    for col in track.columns:
-        track[col] = track[col].astype(float)
+        lat_col = next(column for column in self.track_col_names if 'lat' in column.lower())
+        lon_col = next(column for column in self.track_col_names if 'lon' in column.lower())
 
-    fig = plt.figure(figsize=figsize)
-    ax= plt.axes(projection=ccrs.PlateCarree())
-    ax.set_extent([ # type: ignore
-        track[lon_col].min() - lon_pad,
-        track[lon_col].max() + lon_pad,
-        track[lat_col].min() - lat_pad,
-        track[lat_col].max() + lat_pad,
-    ], crs=ccrs.PlateCarree())
+        self.track = self.source_df[[lon_col, lat_col]].dropna()
 
-    ax.add_feature(cfeature.LAND, facecolor="0.9") # type: ignore
-    ax.add_feature(cfeature.OCEAN, facecolor="lightblue") # type: ignore
-    ax.coastlines(resolution="10m") # type: ignore
-    ax.add_feature(cfeature.BORDERS, linestyle=":") # type: ignore
-    gridlines = ax.gridlines(draw_labels=True, linestyle="--", alpha=0.5) # type: ignore
-    gridlines.top_labels = False
-    gridlines.right_labels = False
+        for col in self.track.columns:
+            self.track[col] = self.track[col].astype(float)
 
-    return fig, ax, track
+        self.ax.set_extent([ # type: ignore
+            self.track[lon_col].min() - self.lon_pad,
+            self.track[lon_col].max() + self.lon_pad,
+            self.track[lat_col].min() - self.lat_pad,
+            self.track[lat_col].max() + self.lat_pad,
+        ], crs=ccrs.PlateCarree())
+
+        return self
+
+    def add_layers(self) -> Self:
+        
+        self.ax.add_feature(cfeature.LAND, facecolor="0.9") # type: ignore
+        self.ax.add_feature(cfeature.OCEAN, facecolor="lightblue") # type: ignore
+        self.ax.coastlines(resolution="10m") # type: ignore
+        self.ax.add_feature(cfeature.BORDERS, linestyle=":") # type: ignore
+        gridlines = self.ax.gridlines(draw_labels=True, linestyle="--", alpha=0.5) # type: ignore
+        gridlines.top_labels = False
+        gridlines.right_labels = False
+
+        return self
+
+    def add_track(self, track_title: str) -> Self:
+        lat_col = next(column for column in self.track_col_names if 'lat' in column.lower())
+        lon_col = next(column for column in self.track_col_names if 'lon' in column.lower())
+
+        if(self.from_vehicle):
+            self.ax.plot(self.track[lon_col], self.track[lat_col], color="tab:red", linewidth=2, transform=ccrs.PlateCarree())
+            self.ax.scatter(self.track[lon_col].iloc[0], self.track[lat_col].iloc[0], color="green", s=60, label="Start", transform=ccrs.PlateCarree())
+            self.ax.scatter(self.track[lon_col].iloc[-1], self.track[lat_col].iloc[-1], color="black", s=60, label="End", transform=ccrs.PlateCarree())
+        else:
+            self.ax.scatter(self.track[lon_col], self.track[lat_col], color="tab:red", linewidth=2, transform=ccrs.PlateCarree())
+
+        self.ax.set_title(track_title)
+        self.ax.legend()
+
+        return self
+
+    def build(self) -> tuple[Figure, Axes]:
+        return self.fig, self.ax, self.track
